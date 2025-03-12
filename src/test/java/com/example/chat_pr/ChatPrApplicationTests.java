400d4a035d9e00ed2485f986601e40b9dcc80d05
saul <dlathf3210@naver.com>
2025-03-12 20:21:41 +0900

    [START]

---
+++ src/test/java/com/example/chat_pr/ChatPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
